35b086c56d09e074f0edb448c50392d5ed0c5b5b
Michael <i@prsly.ru>
2018-09-05 00:01:14 +0300

    add switch-case to script, add ip port choose

--- openvpnsetup.py
+++ openvpnsetup.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python
+#
+# Express setup of OpenVPN server 
+# for Ubuntu Server 16.x / 17.x
+# by xl-tech https://github.com/xl-tech
+# fork by prsly https://github.com/prsly
+#
+# Version 0.1.1 16.06.2018
+#
+# Use only on fresh installed machine! It can rewrite your firewall rules
+# or your current OpenVPN config (if you have it before).
+#
+# Script is licensed under the GNU General Public License v3.0
+#
+# Usage: just run openvpnsetup.py :)
+#
 # -*- coding: utf-8 -*-
 
 import os
@@ -5,14 +21,21 @@ import sys
 import subprocess
 import getpass
 
-def select_IP(choose):
-    if choose == 1:
-        return iip
-    if choose == 2:
-        return eip
-    raise: 
-        ValueError('Invalid option')
-        select_IP(input())
+class switch(object):
+    def __init__(self, value):
+        self.value = value
+        self.fall = False   
+    def __iter__(self):
+        yield self.match
+        raise StopIteration
+  
+    def match(self, *args):
+        if self.fall or not args:
+            return True
+        elif self.value in args:
+            self.fall = True
+            return True
+        return False
 
 print("1 --- Checking superuser permission")
 iam = getpass.getuser()
@@ -58,4 +81,31 @@ iipv6,temp = subprocess.Popen("ip -6 addr|grep inet6|grep fe80|awk -F '[ \t]+|' 
 iipv6 = iipv6.decode('utf-8')[0:-1]
 
 print(f"Select server IP to listen on (only used for IPv4):\n 1) Internal IP - {iip} (in case you are behind NAT)\n 2) External IP - {eip}")
-ip = select_IP(input())
+choose = int(input())
+for case in switch(choose):
+    if case(1): 
+        ip = iip
+        break
+    if case(2):
+        ip = eip
+        break
+    if case():
+        print('Invalid option')
+
+print("Select server PORT to listen on:\n" + 
+" 1) tcp 443 (recommended)" + 
+" 2) udp 1194 (default)"
+" 3) Enter manually (proto (lowercase!) port)"
+choose = int(input())
+for case in switch(choose):
+    if case(1):
+        port = "tcp 443"
+    if case(2):
+        port = "udp 1194"
+    if case(3):
+        print("Enter proto and port (like tcp 80 or udp 53): ")
+        port = input()
+    if case():
+        print('Invalid option')
+portl,portn = port.rsplit(" ")
+portl6 = portl + "6"
