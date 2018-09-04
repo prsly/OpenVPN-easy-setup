aed0cf5c5bab8c04a5902b327825763f018c26c8
Michael <i@prsly.ru>
2018-09-04 18:13:28 +0300

    add IPv4 Forwarding

--- openvpnsetup.py
+++ openvpnsetup.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 import getpass
 
 iam = getpass.getuser()
@@ -12,3 +13,13 @@ if (os.path.isdir("/dev/net/tun")):
 else:
     print("TUN/TAP is disabled. Contact your VPS provider to enable it")
     sys.exit(2)
+
+ipv4forward = subprocess.call('sysctl net.ipv4.ip_forward | grep 0', shell=True, stdout=subprocess.PIPE)
+
+if (ipv4forward == 0):
+    subprocess.call('sysctl -w net.ipv4.ip_forward=1', shell=True, stdout=subprocess.PIPE)
+    conf = open('/etc/sysctl.conf', 'a')
+    conf.write('net.ipv4.ip_forward = 1')
+    conf.close()
+else:
+    print("IPv4 forwarding is already enabled")    
